001a470fa06222e567cbd97af03477493c7d2f7a
shuning@simpop <shnxia@126.com>
2024-12-02 14:55:32 +0800

    add a 'PSP.py'

---
+++ PPS/PSP.py
@@ -0,0 +1,14 @@
+
+'''
+This module is for Pre- Post- and Store.
+1, Pre  : read vtk file, transfer to .csv or numpy format
+2, Post : (1) read the data from database, with H5 format. and 
+          (2) write the data a temporary vtk file, which would be displayed in GUI.
+3, Store: Save the .csv file into h5 database.
+
+@author       @date         @aff          @version
+Xia, S        2024.11.2     Simpop.cn     v0.1
+'''
+
+
+
