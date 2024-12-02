1b5ad9141f8338c1704d885d5928f1936c5e9de6
shuning@simpop <shnxia@126.com>
2024-12-02 15:19:16 +0800

    update the header info in PSP.py

--- PSP/PSP.py
+++ PSP/PSP.py
@@ -1,10 +1,10 @@
 
 '''
-This module is for Pre- Post- and Store.
+This module is for Pre, Store and Post submodules.
 1, Pre  : read vtk file, transfer to .csv or numpy format
-2, Post : (1) read the data from database, with H5 format. and 
+2, Store: Save the .csv file into h5 database.
+3, Post : (1) read the data from database, with H5 format. and 
           (2) write the data a temporary vtk file, which would be displayed in GUI.
-3, Store: Save the .csv file into h5 database.
 
 @author       @date         @aff          @version
 Xia, S        2024.11.2     Simpop.cn     v0.1
