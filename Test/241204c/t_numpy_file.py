7007acbf4979bcccb892aef536594fbd5b72c604
shuning@simpop <shnxia@126.com>
2024-12-04 17:53:52 +0800

    new check for numpy.savez

--- Test/241204c/t_numpy_file.py
+++ Test/241204c/t_numpy_file.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-a = np.array([[1,2,3,4],[2,3,4,5]])
+a = np.array([[1,2,3],[3,4,5]])
 
 # write to .np file
 np.save("t_numpy_file.npy", a)
@@ -9,6 +9,6 @@ np.save("t_numpy_file.npy", a)
 # read/recover from .np file
 b = np.load('./t_numpy_file.npy')
 
-print("b = ", b, type(b))
+print("b =\n", b, "\n", type(b))
 
 # for write and load multiple ndarray, one can use savez
