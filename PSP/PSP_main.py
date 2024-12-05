dc842f6a1f9a07af47f3067e47e9377caca26f2b
shuning@simpop <shnxia@126.com>
2024-12-05 17:26:37 +0800

    already to recognize each vtm file in all cases in a loop manner

--- PSP/PSP_main.py
+++ PSP/PSP_main.py
@@ -14,8 +14,11 @@ Xia, S        2024.11.5     Simpop.cn     v1.0
 
 from pathlib import Path
 
-# Give the .vtm file's path
-CasesPath = Path("../FSCases")
+# register all cases names to a list
+
+# case indexes
+CommonPath = Path("../FSCases")
+
 numList = [  1,  3,  5,\
             11, 13, 15,\
             21, 23, 25,\
@@ -25,14 +28,29 @@ numList = [  1,  3,  5,\
            101,103,105,\
            111,113,115,\
            121,123,125]
-#print(numList, len(numList))
 
+# number of cases
 NumOfCases = len(numList)
 
+# register each case name to a list of strings
 Cases = []
 for i in range(NumOfCases):
   s = "%03d"%numList[i]
   Cases.append("C"+s)
-  print(Cases[i], type(Cases[i]))
 
+# assertain each case's path
+CasePath = []
+for i in range(NumOfCases):
+  path = CommonPath.joinpath(Cases[i]) 
+  CasePath.append(path)
+  #print(CasePath[i])
+
+for i in range(NumOfCases):
+  vtm = "case" + "%d"%numList[i] + "_point.002000.vtm"
+
+  full_vtm = CasePath[i].joinpath(Path(vtm))
+  #print(full_vtm)
+  #with open()
+
+VTMFileNames = []
 
