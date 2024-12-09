ca0f75a467de013564698926a3a3b26c0a52798f
shuning@simpop <shnxia@126.com>
2024-12-09 09:54:20 +0800

    finish writing vtm file

--- PSP/PSP_HDF2VTK.py
+++ PSP/PSP_HDF2VTK.py
@@ -12,14 +12,15 @@ from pathlib import Path
 from WriteVTM import WriteVTM
 
 numOfBlocks = 8
-dirVTM = Path("./Disp")
+dirVTM = Path("./FNN")
 if not dirVTM.exists(): dirVTM.mkdir()
 #if dirVTM.exists(): dirVTM.rmdir() # 删除一个非空目录
 
 fileVTM = dirVTM.joinpath("t01.vtm")
 
-# todo
+# write the vtm file and return the path of vtr files
 dirVTR = WriteVTM(numOfBlocks, fileVTM)
+print(dirVTR)
 
-dirVTR = dirVTM.joinpath("blocks/")
+dirVTR = dirVTM.joinpath(dirVTR)
 print(dirVTR)
--- PSP/WriteVTM.py
+++ PSP/WriteVTM.py
@@ -3,7 +3,13 @@ from pathlib import Path
 
 def WriteVTM(numOfBlocks:int, fileVTM:Path)->Path:
   vtm = open(fileVTM, 'wb')
-  #vtm.writelines(b"<?xml version=\"1.0\"?>")
+  vtm.write(b'<?xml version="1.0"?>\n')
+  vtm.write(b'<VTKFile type="vtkMultiBlockDataSet" version="1.0" byte_order="LittleEndian">\n')
+  vtm.write(b'  <vtkMultiBlockDataSet>\n')
+  vtm.write(b'    <DataSet index="   1" file="channel/1.vtr"/>\n')
+  vtm.write(b'  </vtkMultiBlockDataSet>\n')
+  vtm.write(b'</VTKFile>')
 
   vtm.close()
-  pass
+
+  return Path("channel")
