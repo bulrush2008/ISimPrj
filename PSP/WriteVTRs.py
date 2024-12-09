51f02c618a14e3aeaf6a2a522ca9ddcb38edb2dd
shuning@simpop <shnxia@126.com>
2024-12-09 14:39:40 +0800

    continue writing all variables in '1.vtr'

--- PSP/WriteVTRs.py
+++ PSP/WriteVTRs.py
@@ -23,15 +23,19 @@ def WriteVTRs(numOfBlocks:int, dirVTR:Path, dirHDF:Path)->None:
   vtr.write(b'    </CellData>\n')
   vtr.write(b'    <PointData>\n')
 
-  # now only write P field
-  vtr.write(b'      <DataArray type="Float64" Name="P" NumberOfComponents="1" format="appended" offset="    0"/>\n')
+  # write P/U/V/W/T field and X/Y/Z
+  vtr.write(b'      <DataArray type="Float64" Name="P" NumberOfComponents="1" format="appended" offset="     0"/>\n')
+  vtr.write(b'      <DataArray type="Float64" Name="U" NumberOfComponents="1" format="appended" offset="116692"/>\n')
+  vtr.write(b'      <DataArray type="Float64" Name="V" NumberOfComponents="1" format="appended" offset="233384"/>\n')
+  vtr.write(b'      <DataArray type="Float64" Name="W" NumberOfComponents="1" format="appended" offset="350076"/>\n')
+  vtr.write(b'      <DataArray type="Float64" Name="T" NumberOfComponents="1" format="appended" offset="466768"/>\n')
   vtr.write(b'    </PointData>\n')
 
   # coordinates are necessary
-  vtr.write(b'    <Coordinates>')
-  vtr.write(b'      <DataArray type="Float64" Name="Xcenter" NumberOfComponents="1" format="appended" offset="    116692"/>\n')
-  vtr.write(b'      <DataArray type="Float64" Name="Ycenter" NumberOfComponents="1" format="appended" offset="    116904"/>\n')
-  vtr.write(b'      <DataArray type="Float64" Name="Zcenter" NumberOfComponents="1" format="appended" offset="    117316"/>\n')
+  vtr.write(b'    <Coordinates>\n')
+  vtr.write(b'      <DataArray type="Float64" Name="Xcenter" NumberOfComponents="1" format="appended" offset=" 583460"/>\n')
+  vtr.write(b'      <DataArray type="Float64" Name="Ycenter" NumberOfComponents="1" format="appended" offset=" 583672"/>\n')
+  vtr.write(b'      <DataArray type="Float64" Name="Zcenter" NumberOfComponents="1" format="appended" offset=" 584084"/>\n')
   vtr.write(b'    </Coordinates>')
   vtr.write(b'  </Piece>\n')
   vtr.write(b'  </RectilinearGrid>\n')
@@ -42,11 +46,42 @@ def WriteVTRs(numOfBlocks:int, dirVTR:Path, dirHDF:Path)->None:
 
   # write pressure field "P"
   fieldP = h5["C001"]["Block-00-P"][:]  # "[:]" is necessary
-  numOfBytes = np.int32((len(fieldP) * 8))
+  numOfBytes = np.int32((len(fieldP)*8))
 
   # write the byte offsets and float loop data
   numOfBytes.tofile(vtr)
   fieldP.tofile(vtr)
+
+  # write U field "U"
+  fieldU = h5["C001"]["Block-00-U"][:]
+  numOfBytes = np.int32(len(fieldU)*8)
+
+  numOfBytes.tofile(vtr)
+  fieldU.tofile(vtr)
+
+  # write pressure field "V"
+  fieldV = h5["C001"]["Block-00-V"][:]  # "[:]" is necessary
+  numOfBytes = np.int32((len(fieldV)*8))
+
+  # write the byte offsets and float loop data
+  numOfBytes.tofile(vtr)
+  fieldV.tofile(vtr)
+
+  # write pressure field "W"
+  fieldW = h5["C001"]["Block-00-W"][:]  # "[:]" is necessary
+  numOfBytes = np.int32((len(fieldW)*8))
+
+  # write the byte offsets and float loop data
+  numOfBytes.tofile(vtr)
+  fieldW.tofile(vtr)
+
+  # write pressure field "T"
+  fieldT = h5["C001"]["Block-00-T"][:]  # "[:]" is necessary
+  numOfBytes = np.int32((len(fieldT)*8))
+
+  # write the byte offsets and float loop data
+  numOfBytes.tofile(vtr)
+  fieldT.tofile(vtr)
 
   # write coords X
   coordsX = h5["C001"]["Block-00-X"][:]
