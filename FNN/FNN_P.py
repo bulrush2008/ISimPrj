b60dfb213cd7dae2ac3973d7289bda16bc7a5bd7
shuning@simpop <shnxia@126.com>
2024-12-11 16:17:55 +0800

    finish splitting the data into train/22 and test/5

--- FNN/FNN_P.py
+++ FNN/FNN_P.py
@@ -23,18 +23,23 @@ from torch.utils.data import Dataset
 class FSimDataset(Dataset):
   def __init__(self, file:Path, caseList:list):
     """
-    The data are remain store in h5 file, read when needed
+    - The data are remain store in h5 file, read when needed
 
+    inputs:
     /caseList/: list of cases names, made of set either "test" or "train"
     """
     self.dataFile = h5py.File(file, 'r')
     self.caseList = caseList
+    self.numCases = len(caseList)
 
-  def __len__(self, idx):
+  def __len__(self):
+    return self.numCases
+
+  def __getitem__(self, idx):
     """
     return the input params and Pressure field
     """
-    if idx >= self.num:
+    if idx >= numOfCases:
       raise IndexError
 
     data = []
@@ -42,9 +47,42 @@ class FSimDataset(Dataset):
       key = "Block-"+ "%02d"%blk + "-P"
       presFieldBlk = list(dataFile[caseList[i]][key][:])
       data += presFieldBlk
+      pass
 
     return np.array(data)
+  
+  def plotVTK(self, index):
+    if idx >= numOfCases:
+      raise IndexError
+    #TODO
+    pass
 
-    
+# split the data, 27 = 22 + 5
+
+numOfCases = 27
+ratioTest = 0.2
+
+sizeOfTestSet = np.int64(numOfCases * ratioTest)
+
+np.random.seed(42)
+permut = np.random.permutation(numOfCases)
+
+from idxList import idxList
+
+listTestCase = []
+for i in permut[:sizeOfTestSet]:
+  theCase = "C" + "%03d"%idxList[i]
+  listTestCase.append(theCase)
+
+#print(listTestCase, len(listTestCase))
+
+listTrainCase = []
+for i in permut[sizeOfTestSet:]:
+  theCase = "C" + "%03d"%idxList[i]
+  listTrainCase.append(theCase)
+
+#print(len(listTrainCase))
+#print(listTrainCase)
+
 
 
--- FNN/quickIdea.md
+++ FNN/quickIdea.md
@@ -6,3 +6,6 @@
 
 2, the field data may need to be normalize before training
   24.12.10, 16:03
+
+3, the np.tofile() may may contain defects regarding the accuracy of the data
+  24.12.11, 15:49
