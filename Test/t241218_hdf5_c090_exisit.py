6dee5e730b68c4d33a37deacc189fddbba5cedd7
shuning@simpop <shnxia@126.com>
2024-12-18 18:22:56 +0800

    input paprameters updated

---
+++ Test/t241218_hdf5_c090_exisit.py
@@ -0,0 +1,12 @@
+
+import h5py
+
+from pathlib import Path
+
+h5FileDir = Path("../FSCases/FSHDF/MatrixData.h5")
+
+#aLive = h5FileDir.exists()
+#print(aLive)
+
+h5 = h5py.File(h5FileDir)
+print(h5.keys())
