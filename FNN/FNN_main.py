15d925fbe02e3d12f820937dbb1b304267f17369
shuning@simpop <shnxia@126.com>
2024-12-19 16:50:48 +0800

    add main function fnn_main.py which will call train_p/u/u/w/T

---
+++ FNN/FNN_main.py
@@ -0,0 +1,14 @@
+
+"""
+This is main function to call:
+  - train_p.py,
+  - train_t.py,
+  - train_u.py,
+  - train_v.py, and
+  - train_w.py
+"""
+
+from train_p import train_p
+
+iSuccess = train_p()
+print("Train Pres Successed? ", iSuccess)
--- FNN/train_p.py
+++ FNN/train_p.py
@@ -17,72 +17,62 @@ from Common.idxList     import idxList, numOfAllCases
 from Common.Regression  import Regression
 from Common.FSimDataset import FSimDataset
 
+def train_p()->bool:
+  iSuccess = False
+  # ----------------- 分割并确定训练数据表、测试数据表 ----------------------
+  # split the data, 49 = 40 + 9
+  ratioTest = 0.2
 
-# ----------------- 分割并确定训练数据表、测试数据表 ----------------------
-# split the data, 49 = 40 + 9
-ratioTest = 0.2
+  sizeOfTestSet = np.int64(numOfAllCases * ratioTest)
 
-sizeOfTestSet = np.int64(numOfAllCases * ratioTest)
+  # 42 是随机种子，其它整数也可以
+  np.random.seed(42)
+  permut = np.random.permutation(numOfAllCases)
 
-# 42 是随机种子，其它整数也可以
-np.random.seed(42)
-permut = np.random.permutation(numOfAllCases)
+  listTestCase = []
+  for i in permut[:sizeOfTestSet]:
+    theCase = "C" + "%03d"%idxList[i]
+    listTestCase.append(theCase)
 
-listTestCase = []
-for i in permut[:sizeOfTestSet]:
-  theCase = "C" + "%03d"%idxList[i]
-  listTestCase.append(theCase)
+  listTrainCase = []
+  for i in permut[sizeOfTestSet:]:
+    theCase = "C" + "%03d"%idxList[i]
+    listTrainCase.append(theCase)
 
-listTrainCase = []
-for i in permut[sizeOfTestSet:]:
-  theCase = "C" + "%03d"%idxList[i]
-  listTrainCase.append(theCase)
+  # ------------------ 数据类的初始化 ---------------------
+  filePathH5 = Path("../FSCases/FSHDF/MatrixData.h5")
+  #aLive = filePathH5.exists()
+  #print(aLive)
 
-# ------------------ 数据类的初始化 ---------------------
-filePathH5 = Path("../FSCases/FSHDF/MatrixData.h5")
-#aLive = filePathH5.exists()
-#print(aLive)
+  fsDataset_train = FSimDataset(filePathH5, listTrainCase)
 
-fsDataset_train = FSimDataset(filePathH5, listTrainCase)
+  # ------------ 生成一个回归模型对象，并执行训练 ----------------
+  R = Regression()
 
-# ------------ 生成一个回归模型对象，并执行训练 ----------------
-R = Regression()
-
-# train the model
-epochs = 1
-
-for i in range(epochs):
-  print("Training Epoch", i+1, "of", epochs)
-
-  for bc, label, _ in fsDataset_train:
-    #print(bc)
-    R.train(bc, label)
+  # train the model
+  epochs = 1
+  for i in range(epochs):
+    print("Training Epoch", i+1, "of", epochs)
+    for bc, label, _ in fsDataset_train:
+      #print(bc)
+      R.train(bc, label)
+      pass
     pass
-  pass
 
-# ---------- 训练完毕，绘制损失函数历史 ----------------------
-DirPNG = Path("./Pics")
-R.saveLossHistory2PNG(DirPNG)
+  # ---------- 训练完毕，绘制损失函数历史 ----------------------
+  DirPNG = Path("./Pics")
+  R.saveLossHistory2PNG(DirPNG)
 
-# ------------- 预测，并于测试集比较 -------------------------
-fsDataset_test = FSimDataset(filePathH5, listTestCase)
-#print(listTestCase)
+  # ------------- 预测，并于测试集比较 -------------------------
+  fsDataset_test = FSimDataset(filePathH5, listTestCase)
 
-#len_test = fsDataset_test.numCases
-# for C034
-inp, pField, coords = fsDataset_test[0]
-#print(fsDataset_test.caseList)
-#print(type(inp), type(pField))
-print(len(coords["x"]))
-print(coords["x"])
+  # for C034
+  inp, pField, coords = fsDataset_test[0]
 
-#outPresTorch = R.forward(inp)
+  # 先预测数据，再写到 HDF5 文件中
+  # 坐标是可选输入数据
+  R.write2HDF(inp, Path("./fnn.h5"), coords=coords)
 
-#outPres = outPresTorch.detach().numpy()
-
-#print(outPres[100])
-#print(type(outPres))
-
-# -------------------- 将预测数据写道 HDF5 文件中 -------------------
-R.write2HDF(inp, Path("./fnn.h5"),coords=coords)
+  iSuccess = True
+  return iSuccess
 
