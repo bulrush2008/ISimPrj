d5feedf9c5ea5745cd170f61de17a44134b2ded5
shuning@simpop <shnxia@126.com>
2024-12-23 11:19:04 +0800

    fnn_main.py updated

--- FNN/FNN_main.py
+++ FNN/FNN_main.py
@@ -12,30 +12,17 @@ Xia, S      24.12.19    Simpop.cn   v3.x
 
 import h5py
 
-from train import train
+from Common.CaseSet import CaseSet
+
+#from train import train
 
 #------------------------------------------------------------------------------
 # case names list of train set and test set
-# split the data, 49 = 40 + 9
+# split the data, 49 = 39 + 10
 ratioTest = 0.2
+caseSet = CaseSet( ratio=ratioTest )
 
-sizeOfTestSet = np.int64( numOfAllCases*ratioTest )
-
-# 42 is random seed，other ints also work
-np.random.seed(42)
-permut = np.random.permutation( numOfAllCases )
-
-# names list: case list consist of train set
-listTestCase = []
-for i in permut[:sizeOfTestSet]:
-  theCase = "C" + "%03d"%idxList[i]
-  listTestCase.append(theCase)
-
-# names list: case list consist of test set
-listTrainCase = []
-for i in permut[sizeOfTestSet:]:
-  theCase = "C" + "%03d"%idxList[i]
-  listTrainCase.append(theCase)
+trnSet, tstSet = caseSet.splitSet()
 
 # create a new empty h5 file
 h5 = h5py.File("./fnn.h5", 'w')
@@ -44,8 +31,8 @@ h5.close()
 #------------------------------------------------------------------------------
 # train the fields one has assigned, which must belong in ["P", "T", "U", "V", "W"]
 
-iSuccess = train( numOfEpochs=1 )
-print("Train Pres Successed? ", iSuccess)
+#iSuccess = train( numOfEpochs=1 )
+#print("Train Pres Successed? ", iSuccess)
 
 #------------------------------------------------------------------------------
 # predict by the trained FNN model
