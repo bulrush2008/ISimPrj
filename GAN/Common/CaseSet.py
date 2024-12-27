18d8a1a08af2003299bd4cce7c2c2751651c0f7c
shuning@simpop <shnxia@126.com>
2024-12-27 16:43:54 +0800

    continue to code GAN ...

---
+++ GAN/Common/CaseSet.py
@@ -0,0 +1,136 @@
+
+import numpy as np
+
+class CaseSet(object):
+  def __init__(self, ratio=0.2):
+    # all cases list
+    self.idxList = [1,   3,   5,   8,   11,  13,  15,
+                    18,  21,  23,  25,  28,  31,  34,
+                    37,  40,  43,  46,  49,  51,  53,
+                    55,  58,  61,  63,  65,  68,  71,
+                    73,  75,  78,  81,  84,  87,  90,
+                    93,  96,  99,  101, 103, 105, 108,
+                    111, 113, 115, 118, 121, 123, 125]
+    
+    self.size = len(self.idxList)
+    self.ratio = ratio
+
+    # info of each case
+    self.blockNumb = 8
+
+    self.blockInfo = []
+    self.blockPosi = []
+    
+    self._calcBlockInfo()
+
+  def __len__( self ):
+    """
+    Now len[obj] is ok
+    """
+    return self.size
+
+  def __getitem__(self, idx):
+    """
+    so you use the class as: obj[idx]
+    """
+    if idx >= self.size:
+      raise IndexError(f"{idx} beyond range.")
+    return self.idxList[idx]
+
+  def _calcBlockInfo(self):
+    calcPtsNum = lambda l: (l[1]-l[0]+1) * (l[3]-l[2]+1) * (l[5]-l[4]+1)
+
+    # for block 0
+    ptsB0 = [2,27,2,52,2,12]
+    numbPtsB0 = calcPtsNum(ptsB0)
+
+    self.blockInfo.append(ptsB0)
+    self.blockPosi.append(numbPtsB0 + 0)
+
+    # for block 1
+    ptsB1 = [2,27,2,52,2,13]
+    numbPtsB1 = calcPtsNum(ptsB1)
+
+    self.blockInfo.append(ptsB1)
+    self.blockPosi.append(numbPtsB1 + self.blockPosi[0])
+
+    # for block 2
+    ptsB2 = [2,27,2,53,2,12]
+    numbPtsB2 = calcPtsNum(ptsB2)
+
+    self.blockInfo.append(ptsB2)
+    self.blockPosi.append(numbPtsB2 + self.blockPosi[1])
+
+    # for block 3
+    ptsB3 = [2,27,2,53,2,13]
+    numbPtsB3 = calcPtsNum(ptsB3)
+
+    self.blockInfo.append(ptsB3)
+    self.blockPosi.append(numbPtsB3 + self.blockPosi[2])
+
+    # for block 4
+    ptsB4 = [2,28,2,52,2,12]
+    numbPtsB4 = calcPtsNum(ptsB4)
+
+    self.blockInfo.append(ptsB4)
+    self.blockPosi.append(numbPtsB4 + self.blockPosi[3])
+
+    # for block 5
+    ptsB5 = [2,28,2,52,2,13]
+    numbPtsB5 = calcPtsNum(ptsB5)
+
+    self.blockInfo.append(ptsB5)
+    self.blockPosi.append(numbPtsB5 + self.blockPosi[4])
+
+    # for block 6
+    ptsB6 = [2,28,2,53,2,12]
+    numbPtsB6 = calcPtsNum(ptsB6)
+
+    self.blockInfo.append(ptsB6)
+    self.blockPosi.append(numbPtsB6 + self.blockPosi[5])
+
+    # for block 7
+    ptsB7 = [2,28,2,53,2,13]
+    numbPtsB7 = calcPtsNum(ptsB7)
+
+    self.blockInfo.append(ptsB7)
+    self.blockPosi.append(numbPtsB7 + self.blockPosi[6])
+    pass
+
+  def splitSet(self):
+    """
+    Split the caseSet into train and test sets
+    """
+    numbOfTrnSet = np.int64( self.size * (1.0-self.ratio) )
+
+    # 1984 is a seed, other ints also OK
+    np.random.seed(1984)
+    permut = np.random.permutation( self.size )
+
+    # give the case names list in train set
+    trnSet = []
+    for i in permut[:numbOfTrnSet]:
+      theCase = "C" + "%03d"%(self.idxList[i])
+      trnSet.append(theCase)
+
+    # give the case names list in test set
+    tstSet = []
+    for i in permut[numbOfTrnSet:]:
+      theCase = "C" + "%03d"%(self.idxList[i])
+      tstSet.append(theCase)
+
+    return trnSet, tstSet
+
+if __name__=="__main__":
+
+  cases = CaseSet( ratio=0.2 )
+
+  #print(len(cases), cases.size)
+  #print(cases[4], cases[43]) # 11, 113
+
+  #listTrn, listTst = cases.splitSet()
+  #print(listTrn, "\n", listTst)
+  print(cases.blockNumb)
+  for iblk in range(cases.blockNumb):
+    print(cases.blockInfo[iblk])
+    print(cases.blockPosi[iblk])
--- GAN/GAN.py
+++ GAN/GAN.py
@@ -11,11 +11,46 @@ This is main function to call:
 Xia, S      24.12.27    Simpop.cn   v2.x
 """
 
-class GAN(obj):
+from pathlib import Path
+
+from Common.CaseSet import CaseSet
+
+class GAN(object):
+#==============================================================================
   def __init__(self):
+  #----------------------------------------------------------------------------
+    # split the cases into train and test sets
+    # now: 49 = 39 + 10
+    ratioTest = 0.2
+    caseSet = CaseSet( ratio=ratioTest )
+
+    trnSet, tstSet = caseSet.splitSet()
+
+    self.trnSet = trnSet
+    self.tstSet = tstSet
+
+    # path of data used as training and possibly test
+    self.filePathH5 = Path("../FSCases/FSHDF/MatrixData.h5")
     pass
+
   def train(self):
+  #----------------------------------------------------------------------------
     pass
+
   def predict(self):
-    pass
-  pass
+  #----------------------------------------------------------------------------
+    pass  # member func predict end
+  pass  # class GAN end
+
+if __name__=="__main__":
+  # create a model to train and predict
+  gan = GAN()
+
+  # Start training the models
+  print("---------- Train ----------")
+  gan.train()
+
+  print("---------- Eval  ----------")
+  gan.predict()
+
+
