2c16e1312f04153efdf0a35d4ef440dd73e7a268
shuning@simpop <shnxia@126.com>
2024-12-27 16:36:28 +0800

    again write GAN V2.X: 1st step

---
+++ GAN/GAN.py
@@ -0,0 +1,21 @@
+
+
+"""
+This is main function to call:
+  - split the data into train and test sets
+  - train,
+  - predict,
+  - save to the database: .h5
+
+@author     @data       @aff        @version
+Xia, S      24.12.27    Simpop.cn   v2.x
+"""
+
+class GAN(obj):
+  def __init__(self):
+    pass
+  def train(self):
+    pass
+  def predict(self):
+    pass
+  pass
