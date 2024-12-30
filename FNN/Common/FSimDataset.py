10db53eae0054bbbddb8b3bcb6eda144031a9a30
shuning@simpop <shnxia@126.com>
2024-12-30 19:55:59 +0800

    some improper comments deleted

--- FNN/Common/FSimDataset.py
+++ FNN/Common/FSimDataset.py
@@ -46,16 +46,9 @@ class FSimDataset(Dataset):
 
     data = []
 
-    # 字典，三个键值："x","y","z"
-    # 每个键后面，都连接这个二维列表数据，分别表示 8 个 block 的坐标数据
-    #coords = {}
-
     # coords["x"] 将会包含所有的 block 的 x 坐标值，
     # coords["y"] 和 coords["z"] 同样如此
     coords = {"x":[], "y":[], "z":[]}
-    #coords["x"] = []  # 2d list
-    #coords["y"] = []  # ..
-    #coords["z"] = []  # ..
 
     for blk in range(8):
       key = "Block-"+ "%02d"%blk + "-" + self.varName
