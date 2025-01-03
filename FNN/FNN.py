4dbaaf0d5818efebc75868cf2ea208d7997c625d
shuning@simpop <shnxia@126.com>
2025-01-03 16:22:16 +0800

    ...

--- FNN/FNN.py
+++ FNN/FNN.py
@@ -42,7 +42,7 @@ class FNN(object):
     # train the fields one has assigned, which must be in
     # ["P", "T", "U", "V", "W"]
     # the order in list does not matter
-    fieldList = {"T":2, "V":2, "P":2, "U":2, "W":2}
+    fieldList = {"T":1, "V":1}
 
     print(f"*Fields Models Will Be Trained with Epochs {fieldList}.")
 
