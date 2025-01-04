24c434caa1d9f1478a9ea310c7048c4339c621d5
shuning@debxii <shnxia@126.com>
2025-01-04 13:46:41 +0800

    in test: dataFrame to list

--- Test/t250104_openpyxl/read_xlsx.py
+++ Test/t250104_openpyxl/read_xlsx.py
@@ -3,12 +3,19 @@ import pandas as pd
 import openpyxl
 
 # 读取 Excel 文件
-excel_file = "ROM工况（包含第三次提交）.xlsx"
+excel_file = "ROM-Cases-Records.xlsx"
 
 df = pd.read_excel(excel_file)
 
-# 将数据保存为 CSV 文件
-csv_file = 'data.csv'
-df.to_csv(csv_file, index=False)
+#df.info()
 
-print(f"Excel 文件已成功转换为 {csv_file}")
+#print(type(df.iloc[20:145,1:4]))
+l = df.iloc[20:145,1:4].values.tolist()
+
+length = len(l); print(length)
+
+# 将数据保存为 CSV 文件
+#csv_file = 'data.csv'
+#df.to_csv(csv_file, index=False)
+
+#print(f"Excel 文件已成功转换为 {csv_file}")
