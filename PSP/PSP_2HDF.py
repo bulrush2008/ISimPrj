5778f561f940b0180f4af47a6bdbb2f1157c618d
shuning@simpop <shnxia@126.com>
2025-01-17 11:53:10 +0800

    PSP split into 2: 2HDF & 2VTK; 2HDF now is checked

---
+++ PSP/PSP_2HDF.py
@@ -0,0 +1,153 @@
+
+"""
+The only main class in PSP module
+
+- "P-": Preconditioning
+  read the vtk file (.vtm, .vtr), converting to structured data;
+- "S-": Save/Store
+  build the hdf5 file to save matrix data. The data would be directly read by
+  both the FNN and GAN model;
+- "P-": Postprocessing
+  read the matrix data, and then convert it to VTK format, which can be
+  displayed by Paraview
+
+@author       @date         @aff          @version
+Xia, S        2025.1.17     Simpop.cn     v5.x
+"""
+
+from pathlib import Path
+import numpy as np
+import h5py
+
+from Common.readVTR import readVTR
+from Common.readVTM import readVTM
+from Common.assertFileExist import assertFileExist
+from Common.caseList import PSP_read_json
+from Common.paramInps import paramInpDict, lenParamInp
+from Common.cleanBadSpots import cleanBadSpots
+from Common.splitData import splitData
+
+class PSP(object):
+#==============================================================================
+  def __init__(self, inpFile:Path=None, outDir:Path=None):
+  #----------------------------------------------------------------------------
+    self.inpF = inpFile
+    self.outD = outDir
+    pass
+
+  def act(self):
+    inpF = self.inpF
+    outPath = self.outD
+
+    self._VTK2HDF(inpPath=inpF,outPath=outPath)
+
+  def _VTK2HDF(self, inpPath:Path=Path("./PSP.json"), outPath:Path=Path("../FSCases/FSHDF")):
+  #----------------------------------------------------------------------------
+    """
+    Get data from the vtk files and write them to hdf5 file, which serve as a
+    database.
+    """
+    inpJSON = inpPath
+
+    caseList = PSP_read_json(inpJSON)
+
+    numOfCases = len(caseList)
+
+    # check the case number
+    if numOfCases != lenParamInp:
+      raise ValueError(f"{numOfCases} must equal to {lenParamInp}")
+
+    # Cases dir and name
+    # all cases are in the directory:
+    caseDir = Path("../FSCases")
+
+    # assertain each case's path
+    casePaths = []
+    for case in caseList:
+      path = caseDir.joinpath(case) 
+      casePaths.append(path)
+      pass
+
+    # MatrixData's directory, the data are integrated with HDF5 format
+
+    # MatrixData dir and name
+    h5Path = outPath
+    if not h5Path.exists(): h5Path.mkdir(parents=True)
+
+    h5File = h5Path.joinpath("MatrixData.h5")
+
+    # open the hdf5 file
+    hdf = h5py.File(h5File, 'w')
+
+    # loop over each case
+    #for iCase in range(numOfCases):
+    for iCase, case in enumerate(caseList):
+      fileNameVTM = Path("case" + "%d"%(iCase+1) + "_point.002000.vtm")
+      filePathVTM = casePaths[iCase].joinpath(fileNameVTM)
+      #print(caseNames[i])
+
+      grpC = hdf.create_group(case)
+      grpC.create_dataset("InParam", data=paramInpDict[case])
+
+      # assertain each vtm file is alive
+      alive = assertFileExist(filePathVTM)
+      if not alive:
+        raise LookupError(f"{filePathVTM} Does Not Exsit.")
+
+      # read the only vtm file in this case
+      numOfBlock, filePathVTR = readVTM(filePathVTM, iCase+1)
+
+      # For certain case, loop all its vtr files, each of which relates to a block
+      for jVTR in range(numOfBlock):
+        theVTRFile = casePaths[iCase].joinpath(filePathVTR[jVTR].decode("ASCII"))
+
+        alive = assertFileExist(theVTRFile)
+        if not alive:
+          raise LookupError(f"{theVTRFile} Does Not Exist.")
+
+        ( fieldP,
+          fieldU,
+          fieldV,
+          fieldW,
+          fieldT,
+          coordsX,
+          coordsY,
+          coordsZ,
+          gIndexRange ) = readVTR(theVTRFile)
+
+        cleanBadSpots(field=fieldP, gIndexRange=gIndexRange)
+        cleanBadSpots(field=fieldT, gIndexRange=gIndexRange)
+
+        # add field data
+        grpC.create_dataset("Block-"+"%02d"%jVTR + "-P", data=fieldP)
+        grpC.create_dataset("Block-"+"%02d"%jVTR + "-U", data=fieldU)
+        grpC.create_dataset("Block-"+"%02d"%jVTR + "-V", data=fieldV)
+        grpC.create_dataset("Block-"+"%02d"%jVTR + "-W", data=fieldW)
+        grpC.create_dataset("Block-"+"%02d"%jVTR + "-T", data=fieldT)
+
+        # add coordinates
+        grpC.create_dataset("Block-"+"%02d"%jVTR + "-X", data=coordsX)
+        grpC.create_dataset("Block-"+"%02d"%jVTR + "-Y", data=coordsY)
+        grpC.create_dataset("Block-"+"%02d"%jVTR + "-Z", data=coordsZ)
+        pass
+      pass
+
+    # close the matrix data file
+    hdf.close()
+    pass
+
+if __name__=="__main__":
+  import sys
+
+  inpDir = Path(sys.argv[1])
+  outDir = Path(sys.argv[2])
+
+  print(sys.argv)
+
+  psp = PSP(inpFile=inpDir, outDir=outDir)
+
+  # 仅打印提示i信息
+  print("Now We Convert All Cases to MatrixData.")
+
+  # 执行动作
+  psp.act()
