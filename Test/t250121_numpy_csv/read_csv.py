baac88cc207a7e0c303718c3bec6ebce55b1b1fa
shuning@simpop <shnxia@126.com>
2025-01-22 09:43:18 +0800

    numpy.loadtxt and 中文注释处理

--- Test/t250121_numpy_csv/read_csv.py
+++ Test/t250121_numpy_csv/read_csv.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
-data = np.loadtxt("./data.csv", delimiter=",", comments="#")
+# 如果 'data.csv' 注释中，出现中文，那么比如制定编码方式，比如这里的 utf-8
+data = np.loadtxt("./data.csv", delimiter=",", comments="#", encoding="utf-8")
 
 print(data)
