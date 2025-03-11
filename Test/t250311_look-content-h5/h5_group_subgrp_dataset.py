87d4fc53fd7e7189a72671a94520825185e80669
shuning@simpop <shnxia@126.com>
2025-03-11 18:06:27 +0800

    check h5 for multi-group and dataset

--- Test/t250311_look-content-h5/h5_group_subgrp_dataset.py
+++ Test/t250311_look-content-h5/h5_group_subgrp_dataset.py
@@ -1,12 +1,16 @@
 
 import pathlib
 import h5py
+import numpy as np
 
 h5f = h5py.File(pathlib.Path("./temp.h5"), mode='w')
 
 grp1 = h5f.create_group("g1")
+grp1.create_dataset(name="d1", data=np.array([1,2,3]), dtype=float)
 sgrp1 = grp1.create_group("sg1")
+sgrp1.create_dataset(name="d12", data=np.array([1,2,3]), dtype=np.float32)
 
 grp2 = h5f.create_group("g2")
+grp2.create_dataset(name="d2", data=np.array([1,2]), dtype=np.int32)
 
 h5f.close()
